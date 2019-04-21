75ad8d9965ab6e6a4fabde00d4a729f4f79076f6
Ian S Pringle <ipringle@protonmail.com>
2019-04-21 00:38:06 -0400

    Initial commit. Player can move around screen. HUZZAHgit add ./!

---
+++ src/main.rs
@@ -0,0 +1,63 @@
+extern crate tcod;
+
+use tcod::console::*;
+use tcod::colors;
+
+const SCREEN_WIDTH: i32 = 80;
+const SCREEN_HEIGHT: i32 = 50;
+const LIMIT_FPS: i32 = 20;
+
+fn main() {
+    let mut root = Root::initializer()
+        .font("static/arial10x10.png", FontLayout::Tcod)
+        .font_type(FontType::Greyscale)
+        .size(SCREEN_WIDTH, SCREEN_HEIGHT)
+        .title("Rusty Rogue")
+        .init();
+    tcod::system::set_fps(LIMIT_FPS);
+
+    let mut player_x = SCREEN_WIDTH / 2;
+    let mut player_y = SCREEN_HEIGHT / 2;
+
+    while !root.window_closed() {
+        root.set_default_foreground(colors::WHITE);
+        root.clear();
+        root.put_char(player_x, player_y, '@', BackgroundFlag::None);
+        root.flush();
+        root.wait_for_keypress(true);
+        root.put_char(player_x, player_y, ' ', BackgroundFlag::None);
+        let exit = handle_keys(&mut root, &mut player_x, &mut player_y);
+        if exit {
+            break
+        }
+    };
+}
+
+fn handle_keys(root: &mut Root, player_x: &mut i32, player_y: &mut i32) -> bool {
+    use tcod::input::Key;
+    use tcod::input::KeyCode::*;
+
+    let key = root.wait_for_keypress(true);
+    match key {
+        //movement keys
+        //Up
+        Key { code: Up, .. } => *player_y -= 1,
+        //Key { printable: k, .. } => *player_y -= 1,
+        Key { code: Down, .. } => *player_y += 1,
+        Key { code: Left, .. } => *player_x -= 1,
+        Key { code: Right, .. } => *player_x += 1,
+
+        //Fullscreen
+        Key { code: Enter, alt: true, .. } => {
+            let fullscreen = root.is_fullscreen();
+            root.set_fullscreen(!fullscreen);
+        }
+
+        //Exit
+        Key { code: Escape, .. } => return true,
+
+        //Catchall
+        _ => {},
+    }
+    false
+}
