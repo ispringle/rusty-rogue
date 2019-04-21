09624a6617e38408437c150ae8a995175ed68153
Ian S Pringle <ipringle@protonmail.com>
2019-04-21 03:46:15 -0400

    Added basic room creation funcs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
+//Rusty Rogue, a roguelike by pard68
+//This game follows the tcod-rs tutorial series
+
 extern crate tcod;
+
+use std::cmp;
 
 use tcod::console::*;
 use tcod::colors::{self, Color};
+
+//CONSTANTS
 
 const SCREEN_WIDTH: i32 = 80;
 const SCREEN_HEIGHT: i32 = 50;
@@ -13,7 +20,11 @@ const COLOR_DARK_GROUND: Color = Color { r: 50, g: 50, b: 150 };
 
 const LIMIT_FPS: i32 = 20;
 
+//TYPES
+
 type Map = Vec<Vec<Tile>>;
+
+//STRUCTS
 
 #[derive(Clone, Copy, Debug)]
 struct Tile {
@@ -66,11 +77,50 @@ impl Object {
     }
 }
 
-fn make_map() -> Map {
-    let mut map = vec![vec![Tile::empty(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
+#[derive(Clone, Copy, Debug)]
+struct Rect {
+    x1: i32,
+    x2: i32,
+    y1: i32,
+    y2: i32,
+}
 
-    map[30][22] = Tile::wall();
-    map[50][22] = Tile::wall();
+impl Rect {
+    pub fn new(x: i32, y: i32, w: i32, h: i32) -> Self {
+        Rect { x1: x, y1: y, x2: x + w, y2: y + h }
+    }
+}
+
+//FUNCTIONS
+
+fn create_h_tunnel(x1: i32, x2: i32, y: i32, map: &mut Map) {
+    for x in cmp::min(x1, x2)..(cmp::max(x1, x2) + 1) {
+        map[x as usize][y as usize] = Tile::empty();
+    }
+}
+
+fn create_v_tunnel(y1: i32, y2: i32, x: i32, map: &mut Map) {
+    for y in cmp::min(y1, y2)..(cmp::max(y1, y2) + 1) {
+        map[x as usize][y as usize] = Tile::empty();
+    }
+}
+
+fn create_room(room: Rect, map: &mut Map) {
+    for x in (room.x1 + 1)..room.x2 {
+        for y in (room.y1 + 1)..room.y2 {
+            map[x as usize][y as usize] = Tile::empty();
+        }
+    }
+}
+
+fn make_map() -> Map {
+    let mut map = vec![vec![Tile::wall(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
+
+    let room1 = Rect::new(20, 15, 10, 15);
+    let room2 = Rect::new(50, 15, 10, 15);
+    create_room(room1, &mut map);
+    create_room(room2, &mut map);
+    create_h_tunnel(25, 55, 23, &mut map);
 
     map
 }
@@ -136,7 +186,7 @@ fn main() {
 
     tcod::system::set_fps(LIMIT_FPS);
 
-    let player = Object::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, '@', colors::WHITE);
+    let player = Object::new(25, 23, '@', colors::WHITE);
     let npc = Object::new(SCREEN_WIDTH / 2 - 5, SCREEN_HEIGHT / 2, '@', colors::YELLOW);
     let mut objects = [player, npc];
 
